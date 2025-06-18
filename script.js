9611b64f4c170d061b109417c69acca98d4b9318
Saurav <jhasaurav593@gmail.com>
2025-06-18 19:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const table = document.getElementById("sampleTable");
 const tr = document.createElement("tr");
       const td = document.createElement("td");
       const td1 = document.createElement("td");
-      td.textContent = "New Cell1"
-      td1.textContent = "New Cell2"
+      td.innerHTML = "New Cell1"
+      td1.innerHTML = "New Cell2"
       tr.appendChild(td);
       tr.appendChild(td1);
       table.prepend(tr);
